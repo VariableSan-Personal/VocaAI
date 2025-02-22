164d6caaabc596cce18adbb4073bcc2fc8574a64
VariableSan <nursultan_nurgaliyev@proton.me>
2025-02-22 11:05:37 +0500

    Disable appManifest in experimental settings of Nuxt configuration

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -4,6 +4,9 @@ import { fileURLToPath, URL } from 'node:url'
 export default defineNuxtConfig({
 	compatibilityDate: '2024-11-01',
 	devtools: { enabled: false },
+	experimental: {
+		appManifest: false,
+	},
 	modules: [
 		'@nuxt/eslint',
 		'@nuxtjs/tailwindcss',
