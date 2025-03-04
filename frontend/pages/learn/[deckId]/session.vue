a57d06448b6665ff1ad9a7641dd4b2fdf6b374c4
VariableSan <nursultan_nurgaliyev@proton.me>
2025-03-04 15:40:43 +0500

    Implement deck learning pages; refactor spaced repetition list and add session view

---
+++ frontend/pages/learn/[deckId]/session.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+	import type { CustomPageMeta } from '~/shared'
+
+	definePageMeta({
+		showBack: true,
+		hideBottomNav: true,
+		title: 'Session',
+	} satisfies CustomPageMeta)
+</script>
+
+<template>
+	<div class="container">
+		<h1>Session</h1>
+	</div>
+</template>
